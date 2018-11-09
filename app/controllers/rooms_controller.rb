1d6cac9973367a553aadfe582e6786bbfd77ed90
wmv1 <willianmarquesv@gmail.com>
2018-11-09 11:28:44 -0200

    Após remover mensagem redireciona para o usuário atual

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,9 @@
 class RoomsController < ApplicationController
     before_action :authenticate_user!
     def create
-        @chat_room = ChatRoom.create
-        Room.create(user: current_user, chat_room: @chat_room)
-        @room = Room.create(user: params['user'], chat_room: @chat_room)
+    #    @chat_room = ChatRoom.create
+    #    Room.create(user: current_user, chat_room: @chat_room)
+    #    @room = Room.create(user: params['user'], chat_room: @chat_room)
         redirect_to  user_rooms_path
     end
     def index
