b78a39b232dff8b2d3983508f96d206a284931fe
wmv1 <willianmarquesv@gmail.com>
2018-11-14 14:29:25 -0200

    Adicionado status on/offline

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -2,14 +2,24 @@
 
 class ChatChannel < ApplicationCable::Channel
   def subscribed
+    current_user.appear
     stream_from "chat_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+p"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:"     
+current_user.disappear
   end
   def speak (data)
+       p "ChatChannel***********"
        Message.create!(content: data['message'], user: current_user,
                        secondary_user: data["user"])
   end
+  def falar (data)
+       p "ChatChannel- Falar***********"
+       Message.create!(content: data['message'], user: current_user,
+                       secondary_user: data["user"])
+  end
+
 end
